4b2e00cf15ee092f2dcb9095ae576198c7f29806
Applikatix <josephhollandhale@gmail.com>
2024-10-29 12:50:43 +0100

    get_energy.py

--- get_energy.py
+++ get_energy.py
@@ -3,36 +3,46 @@ import pandas as pd
 
 # https://api.energidataservice.dk/dataset/ElectricityProdex5MinRealtime?offset=0&start=2020-01-01T00:00&end=2020-12-31T00:00&sort=Minutes5UTC%20DESC
 
-year = 2020
-params = dict(
-    offset=0,
-    start=f"{year}-01-01T00:00",
-    end=f"{year}-12-31T00:00",
-    sort="Minutes5UTC DESC",
-    # limit=100,
-    limit=0
-)
-url = 'https://api.energidataservice.dk/dataset/ElectricityProdex5MinRealtime'
-response = requests.get(
-    url=url,
-    params=params
-)
+def extract(year, limit=0):
+    url = 'https://api.energidataservice.dk/dataset/ElectricityProdex5MinRealtime'
+    params = dict(
+        offset=0,
+        start=f"{year}-01-01T00:00",
+        end=f"{year+1}-01-01T00:00",
+        sort="Minutes5UTC DESC",
+        limit=limit)
+    
+    response = requests.get(url=url, params=params)
+    if (code := response.status_code) != 200:
+        print(code)
+        print(response.text)
+    df = pd.DataFrame(
+        response
+        .json()
+        .get('records', []))
+    return df
 
-if response.status_code != 200:
-    print(response.status_code)
-    print(response.text)
+def load(dataframe: pd.DataFrame):
+    dataframe.to_csv(fname := 'energi_data.csv')
+    return fname
 
-result = response.json()
+def transform(filename):
+    import csv
+    res = dict(DK1=0, DK2=0)
+    with open(filename) as f:
+        for row in csv.DictReader(f):
+            res[row['PriceArea']] += float(row['ExchangeGermany'])
+    return res
 
-records = result.get('records', [])
+def run(year):
+    print('getting data...')
+    dataframe = extract(year)
+    print(dataframe)
+    print('saving...')
+    filename = load(dataframe)
+    print(filename)
+    print('calculating export...')
+    eksport = transform(filename)
+    print(eksport)
 
-print('records:')
-print(len(records))
-
-# gem som ... csv eller json eller ?
-
-energy_df = pd.DataFrame(records)
-print(energy_df)
-
-energy_df.to_csv(f'energi_{year}.csv')
-energy_df.to_json(f'energi_{year}.json')
+run(2023)
